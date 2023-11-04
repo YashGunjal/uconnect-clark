6087d84cbbbcaad420b6dfef8034a3d045ce3467
Yash Gunjal <yashgunjal98@gmail.com>
2023-11-04 18:50:56 -0400

    front:  layout change

--- frontend/web/containers/sections/main/SubjectMain.js
+++ frontend/web/containers/sections/main/SubjectMain.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import { appLoaderKey } from "../../../AppLoaderSlice";
 import { DataLoaderKey } from "../../dataloader/DataloaderSlice";
 import { useSelector, useDispatch } from "react-redux";
@@ -11,6 +11,7 @@ import {
   Card,
   CardBody,
   CardHeader,
+  Container,
   Col,
   Row,
 } from "reactstrap";
@@ -22,16 +23,74 @@ import { toogleSidePanel,subjectskey } from "./SubjectsSlice";
 
 function SubjectMain({ department }) {
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => {
+  const { user, screenDimension } = useSelector((state) => {
     return state[appLoaderKey];
   });
   const { sidePanelOpen } = useSelector((state) => {
     return state[subjectskey];
   });
 
+  const navbarHeight = document.getElementById("navbar-main").clientHeight;
+
+  // console.log(element,element.current, screenDimension)
+  console.log(navbarHeight, " total", screenDimension, screenDimension.height - navbarHeight -5)
+
   const { id, name } = department;
   return (
     <React.Suspense fallback="Loading...">
+      <Container style={{maxWidth: "1440px", height:screenDimension.height - navbarHeight -3 }} className="bg-white w-100">
+      <Row>
+        <Col md="3" xs="12">
+          {/* Left column, 1/4 of the screen on medium and larger screens, and full width on smaller screens */}
+          <div className="pt-2 text-center">
+          <Button
+            color="clark-red "
+            id="toggler"
+            style={{
+              marginBottom: "1rem",
+            }}
+            onClick={()=> { 
+              console.log("dispactching")
+              dispatch(toogleSidePanel())
+            }
+            }
+          >
+            { sidePanelOpen ? "Hide Course List":  "Show All Courses"}
+          </Button>
+          </div>
+          <div>
+          <CourseList  />
+          </div>
+        </Col>
+        <Col md="9" xs="12">
+          {/* Right column, 3/4 of the screen on medium and larger screens, and full width on smaller screens */}
+          <div className="p-2 pl-1 lh-2">
+          <BreadCrumb items={[name, "MS CS", " Software Engineering"]} />
+        
+          </div>
+
+          <div>
+          <ChatWindow />
+          </div>
+          
+        </Col>
+      </Row>
+    </Container>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* 
       <Row className="bg-white">
         <Col className="border pt-2 text-center" xs="3">
           <Button
@@ -61,7 +120,7 @@ function SubjectMain({ department }) {
         <Col className="border pl-0" xs={sidePanelOpen ? "9" : "12"}>
           <ChatWindow />
         </Col>
-      </Row>
+      </Row> */}
     </React.Suspense>
   );
 }
