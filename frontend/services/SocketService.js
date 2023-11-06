783ee5f8c07240cbe1c8a9e3b6d0a6c529a985cc
Yash Gunjal <yashgunjal98@gmail.com>
2023-11-06 14:49:37 -0500

    front: add socket listeners & state update slice

--- frontend/services/SocketService.js
+++ frontend/services/SocketService.js
@@ -1,35 +1,21 @@
 import socket from "./SocketBase";
 
 var SocketService = {
-    sendMessage: function (payload ) {
-        
-        try {
-            socket.emit("send_message",{ ...payload} )
-        }
-        catch (error) {
-            console.error(error);
-        }
-    },
-    receiveMessage: function (setMessages,messages, setCount) {
-        return new Promise(function(resolve, reject) {
-            try {
-                socket.on("receive_message", (data) => {
-                // console.log("socket executed")
-                console.log("service received messge", data,messages, )
-                setMessages((previous) => [...previous, data.message])
-                setCount(Math.random())
-                resolve(data)
-                });  
-            }
-            catch (error) {
-                console.error(error);
-            }
-        })
+  event: function (event, payload) {
+    try {
+      socket.emit(event, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        ...payload,
+      });
+    } catch (error) {
+      console.error(error);
     }
-}
+  },
+  
+};
 export default SocketService;
-
-
 
 // const api = function() {
 //     return new Promise(function(resolve, reject) {
@@ -39,4 +25,3 @@ export default SocketService;
 //       })
 //     });
 //   }
-  
