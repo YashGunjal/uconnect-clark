4ce874e0e2c0fa19fe5e971d8589b490d63ea277
Yash Gunjal <yashgunjal98@gmail.com>
2023-12-04 12:51:18 -0500

    front: populate course and subject on frontend

--- frontend/web/containers/sections/courselist/CouseList.js
+++ frontend/web/containers/sections/courselist/CouseList.js
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 
-
 import { appLoaderKey } from "../../../AppLoaderSlice";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   Alert,
-
   List,
   Collapse,
   Button,
@@ -15,53 +13,59 @@ import {
   CardHeader,
   Modal,
   ModalBody,
-  ModalFooter
+  ModalFooter,
 } from "reactstrap";
 
-import { subjectskey } from "../main/SubjectsSlice"
+import {
+  subjectskey,
+  updateCourse,
+  updateSubject,
+  updateSelectedSubject,
+} from "../main/SubjectsSlice";
+import SubjectServices from "../../../../services/SubjectService";
 
-
-export default  function CourseList() {
-  const [subjectModal, setSubjectModal] = useState(false)
+export default function CourseList() {
+  const dispatch = useDispatch();
+  const [subjectModal, setSubjectModal] = useState(false);
   const [openedCollapses, setOpenedCollapse] = useState([1]);
-  
-  const { sidePanelOpen } = useSelector((state) => {
+
+  const { sidePanelOpen, courses, subjects } = useSelector((state) => {
     return state[subjectskey];
   });
   const { user } = useSelector((state) => {
     return state[appLoaderKey];
   });
 
+  useEffect(async () => {
+    let response = await SubjectServices.getCoursesAndSubjects(1);
+    dispatch(updateCourse(response.courses));
+    dispatch(updateSubject(response.subjects));
 
-  const courselist = [
-    "Algorithms",
-              "Computer Architecture",
-              "Cyber Security"
-  ]
+    console.log("fetch response", response);
+  }, []);
 
-  const showDetails= (e) => {
-    e.preventDefault()
-    setSubjectModal(true)
-  }
+  const showDetails = (e) => {
+    e.preventDefault();
+    setSubjectModal(true);
+  };
 
   const handleClose = () => {
     setSubjectModal(false);
   };
 
   const AccordianContent = () => {
-    
     const collapsesToggle = (collapse) => {
-      console.log(openedCollapses, collapse)
+      console.log(openedCollapses, collapse);
       if (openedCollapses.includes(collapse)) {
         setOpenedCollapse([]);
       } else {
         setOpenedCollapse([collapse]);
       }
     };
-    console.log(openedCollapses)
-    
-    const AccordianCard = ({ title, index }) => (
-      <Card className="card-plain m-0" >
+    console.log(openedCollapses);
+
+    const AccordianCard = ({ title, courseId, index }) => (
+      <Card className="card-plain m-0">
         <CardHeader
           className="m-0 bg-clark-red"
           role="tab"
@@ -69,32 +73,40 @@ export default  function CourseList() {
           aria-expanded={openedCollapses.includes(index)}
           color="clark-red text-white"
           style={{
-            text:"white",
-            color:"white"
+            text: "white",
+            color: "white",
           }}
         >
-          <div className=" d-flex justify-content-between flex-row "> 
-            
-            <h5 className="mb-0 text-white">
-            {title}{" "} </h5>
+          <div className=" d-flex justify-content-between flex-row ">
+            <h5 className="mb-0 text-white">{title} </h5>
             {openedCollapses.includes(index) ? (
               <FaAngleUp />
-              ) : (
-                <FaAngleDown />
-                )}{" "}
-                </div>
+            ) : (
+              <FaAngleDown />
+            )}{" "}
+          </div>
         </CardHeader>
-        <Collapse
-          role="tabpanel"
-          isOpen={openedCollapses.includes(index)}
-        >
+        <Collapse role="tabpanel" isOpen={openedCollapses.includes(index)}>
           <CardBody className="text-dark">
             <List>
-              {courselist.map((course, index) => (
+              {Object.keys(subjects).length > 0 ? (
+                subjects?.[courseId]?.map((subject) => (
+                  <li
+                    className="pointer"
+                    key={index}
+                    onClick={(e) => dispatch(updateSelectedSubject(subject))}
+                  >
+                    {subject.name}
+                  </li>
+                ))
+              ) : (
+                <p>NO Subjects, because Dhuv d </p>
+              )}
+
+              {/* {courselist.map((course, index) => (
                 <li className="pointer" key={index}  onClick={(e)=>showDetails(e)} >{course}</li>
 
-              ))}
-              
+              ))} */}
             </List>
           </CardBody>
         </Collapse>
@@ -104,58 +116,53 @@ export default  function CourseList() {
     return (
       <>
         <div className="accordion m-0">
-          <AccordianCard title={"MS in IT"} inedx={1} />
-          <AccordianCard title={"MS in CS"} index={2}/>
-          <AccordianCard title={"MS in DA"} index={3}/>
+          {Object.keys(courses).length > 0 ? (
+            courses?.map((course, index) => (
+              <AccordianCard
+                title={course.name}
+                courseId={course.id}
+                index={index+1}
+              />
+            ))
+          ) : (
+            <></>
+          )}
         </div>
       </>
     );
   };
 
-
   return (
-      <div>
-        
-        <Collapse  horizontal
-        delay={{ show: 10}}
-        isOpen={sidePanelOpen} 
+    <div>
+      <Collapse
+        horizontal
+        delay={{ show: 10 }}
+        isOpen={sidePanelOpen}
 
-            // style={{
-            //   width: "25%",
-            // }}
-          >
-            <Alert className="p-1"  color="clark-red">
-            <AccordianContent />
-          </Alert>
-            </Collapse>
+        // style={{
+        //   width: "25%",
+        // }}
+      >
+        <Alert className="p-1" color="clark-red">
+          <AccordianContent />
+        </Alert>
+      </Collapse>
 
-
-
-
-<Modal
-          isOpen={subjectModal}
-          toggle={handleClose}
-          onClosed={handleClose}
-        >
-          <ModalBody>
-            <Card>
-
-              <CardHeader> 
-                <p className="h-5"> Course Name </p>
-              </CardHeader>
-              <CardBody> 
-                <p> detail about the subject will be given here</p>
-              </CardBody>
-            </Card>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={handleClose}>Close</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    
+      <Modal isOpen={subjectModal} toggle={handleClose} onClosed={handleClose}>
+        <ModalBody>
+          <Card>
+            <CardHeader>
+              <p className="h-5"> Course Name </p>
+            </CardHeader>
+            <CardBody>
+              <p> detail about the subject will be given here</p>
+            </CardBody>
+          </Card>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={handleClose}>Close</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
   );
 }
-
-
-
